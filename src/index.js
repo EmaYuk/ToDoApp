01800e11bf5bda9fd1bccd388b20c94603c00d0c
EmaYuk <89290400+EmaYuk@users.noreply.github.com>
2021-12-27 00:48:35 +0900

    タスクの追加機能まで実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // liタグの子要素にdivを設定
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
