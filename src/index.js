ceb112ca8739b3d093ae071672517ccf457b6953
EmaYuk <89290400+EmaYuk@users.noreply.github.com>
2021-12-28 02:27:30 +0900

    タスクの戻す機能実装

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  createIncompleteList(inputText);
+};
 
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // liタグ生成
   const li = document.createElement("li");
 
@@ -14,7 +23,7 @@ const onClickAdd = () => {
 
   // pタグ生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -38,10 +47,19 @@ const onClickAdd = () => {
     // button(戻す)タグ作成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(li)を完了リストから削除
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = deleteTarget.firstElementChild.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     // divタグの子要素に各要素を設定
-    addTarget.appendChild(p);
-    addTarget.appendChild(backButton);
+    addTarget.firstElementChild.appendChild(p);
+    addTarget.firstElementChild.appendChild(backButton);
 
     // 完了リストに追加
     document.getElementById("complete-list").appendChild(addTarget);
@@ -65,11 +83,6 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
